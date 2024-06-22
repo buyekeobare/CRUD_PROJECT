195e14341d4e6b3f79a4780c9c00ca64d1441ded
buyekeobare <buyekeobare@gmail.com>
2024-06-22 23:10:24 +0300

    Updated student model file

--- student_records/students/models.py
+++ student_records/students/models.py
@@ -11,4 +11,4 @@ class Student(models.Model):
     hobby = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"student {self.first_name} {self.last_name}"
+        return f"Student {self.first_name} {self.last_name}"
