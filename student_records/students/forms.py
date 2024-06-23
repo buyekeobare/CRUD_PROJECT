f75298d8089eacad9d8369c86044cf1c5ae6dcc7
buyekeobare <buyekeobare@gmail.com>
2024-06-23 16:50:28 +0300

    Updated student form

--- student_records/students/forms.py
+++ student_records/students/forms.py
@@ -4,4 +4,5 @@ from .models import Student
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['name', 'age', 'email', 'phone', 'location', 'hobby']
+        fields = ['first_name', 'last_name', 'age', 'email', 'phone', 'location', 'hobby']
+      
