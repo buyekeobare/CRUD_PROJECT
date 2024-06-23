f127582c666119e96c631fc742ee36a136c10b63
buyekeobare <buyekeobare@gmail.com>
2024-06-23 17:06:05 +0300

    syntax correction in students urls.py

--- student_records/students/urls.py
+++ student_records/students/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create_student, name='create_student'),
-    path('update/<int:pk>/', views.student_update, name='update_student'),
-    path('delete/<int:pk>/', views.student_delete, name='confirm_delete'),
+    path('update/<int:pk>/', views.update_student, name='update_student'),
+    path('delete/<int:pk>/', views.confirm_delete, name='confirm_delete'),
 ]
