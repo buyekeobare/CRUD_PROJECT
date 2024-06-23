b115d363912851c275d0dcd930400a4a29e4970f
buyekeobare <buyekeobare@gmail.com>
2024-06-23 15:29:38 +0300

    student form, list, info created

--- student_records/students/views.py
+++ student_records/students/views.py
@@ -1,17 +1,48 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Student
+from .forms import StudentForm
 
-# Create your views here.
+# Index view to list all students
 def index(request):
-  return render(request, 'students/index.html', {
-    'students': Student.objects.all()
-  })
+    students = Student.objects.all()
+    print(students)  # For debugging purposes
+    return render(request, 'students/index.html', {'students': students})
 
-def view_student(request, id):
-  student = Student.objects.get(pk=id)
-  return HttpResponseRedirect(reverse('index'))
+# Get student information
+def student_info(request, pk):  # pk (primary key; a unique identifier for students)
+    student = get_object_or_404(Student, pk=pk)
+    return render(request, 'students/info.html', {'student': student})
+
+# Create a new student
+def student_create(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = StudentForm()
+    return render(request, 'students/form.html', {'form': form})
+
+# Update an existing student information 
+def student_update(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'students/form.html', {'form': form})
+
+# Deleete and existing student information
+def student_delete(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == "POST":
+        student.delete()
+        return redirect('index')
+    return render(request, 'students/confirm_delete.html', {'student': student})
 
 
 
